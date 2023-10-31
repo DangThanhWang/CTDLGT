c98aad48c57f93a2e3c8a192dd330c5daa4312b6
DangThanhWang <125245112+DangThanhWang@users.noreply.github.com>
2023-10-31 21:06:02 +0700

    week9

---
+++ src/week9/Bai1.java
@@ -0,0 +1,17 @@
+package week9;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MaxPQ;
+import edu.princeton.cs.algs4.SequentialSearchST;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        SequentialSearchST<Object, Object> st = new SequentialSearchST<>();
+        In in = new In("C:\\Users\\LENOVO\\IdeaProjects\\Exam\\algs4-data\\tinyST.txt");
+        String s;
+        while(!in.isEmpty()) {
+            s = in.readString();
+
+        }
+    }
+}
